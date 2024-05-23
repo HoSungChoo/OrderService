08f191daac786a17f8257fd68bec2f2608461787
HSCHOO <57245883+HoSungChoo@users.noreply.github.com>
2024-05-23 23:41:49 +0900

    walletServiceTest 초안 작성

--- order/src/test/java/com/hozzi/order/domain/user/service/WalletServiceTest.java
+++ order/src/test/java/com/hozzi/order/domain/user/service/WalletServiceTest.java
@@ -1,20 +1,51 @@
 package com.hozzi.order.domain.user.service;
 
+import com.hozzi.order.domain.pay.repo.PayRepo;
+import com.hozzi.order.domain.user.repo.UserRepo;
+import com.hozzi.order.domain.user.repo.WalletRepo;
+import com.hozzi.order.domain.user.service.impl.WalletServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WalletServiceTest {
-
-    @Test
-    void readWallet() {
+    private UserRepo userRepo = Mockito.mock(UserRepo.class);
+    private WalletRepo walletRepo = Mockito.mock(WalletRepo.class);
+    private PayRepo payRepo = Mockito.mock(PayRepo.class);
+    private WalletServiceImpl walletService;
+    @BeforeEach
+    public void setUpTest(){
+        walletService = new WalletServiceImpl(userRepo, walletRepo, payRepo);
     }
-
     @Test
+    @DisplayName("")
+    void readWallet_Normal_Success() {
+    }
+    @Test
+    @DisplayName("")
+    void readWallet_NotExistUserId_Exception() {
+    }
+    @Test
+    @DisplayName("")
+    void readWallet_readQuitedUser_Success() {
+    }
+    @Test
+    @DisplayName("")
+    void createWallet_Normal_Success() {
+    }
+    @Test
+    @DisplayName("")
     void createWallet() {
     }
-
     @Test
-    void deleteWallet() {
+    @DisplayName("")
+    void deleteWallet_Normal_Success() {
+    }
+    @Test
+    @DisplayName("")
+    void deleteWallet_NotExistUserId_Exception() {
     }
 }
