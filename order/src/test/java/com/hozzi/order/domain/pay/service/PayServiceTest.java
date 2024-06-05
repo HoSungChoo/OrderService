62e54d0c96d922d7fc0a28c52968afb5f7c88775
HSCHOO <57245883+HoSungChoo@users.noreply.github.com>
2024-06-05 12:12:56 +0900

    payServiceTest 초안 작성

---
+++ order/src/test/java/com/hozzi/order/domain/pay/service/PayServiceTest.java
@@ -0,0 +1,45 @@
+package com.hozzi.order.domain.pay.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PayServiceTest {
+    @Test
+    @DisplayName("")
+    void readPayments_Normal_Success() {
+    }
+    @Test
+    @DisplayName("")
+    void readPayments_Exception_Exception() {
+    }
+    @Test
+    @DisplayName("")
+    void readPayment_Normal_Success() {
+    }
+    @Test
+    @DisplayName("")
+    void readPayment_NotExistPaymentId_Exception() {
+    }
+    @Test
+    @DisplayName("")
+    void createPayment_Normal_Success() {
+    }
+    @Test
+    @DisplayName("")
+    void createPayment_DuplicatedPaymentName_Exception() {
+    }
+    @Test
+    @DisplayName("")
+    void deletePayment_Normal_Success() {
+    }
+    @Test
+    @DisplayName("")
+    void deletePayment_NotExistPaymentId_Exception() {
+    }
+    @Test
+    @DisplayName("")
+    void deletePayment_AlreadyCanceled_Exception() {
+    }
+}
