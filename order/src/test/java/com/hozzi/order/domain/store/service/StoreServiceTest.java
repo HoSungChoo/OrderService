431a29a7d7045ad60b3f814635cb609487d76b1d
HSCHOO <57245883+HoSungChoo@users.noreply.github.com>
2024-06-07 16:06:03 +0900

    storeServiceTest 초안 작성

--- order/src/test/java/com/hozzi/order/domain/store/service/StoreServiceTest.java
+++ order/src/test/java/com/hozzi/order/domain/store/service/StoreServiceTest.java
@@ -1,20 +1,44 @@
 package com.hozzi.order.domain.store.service;
 
+import com.hozzi.order.domain.store.mapper.MenuMapper;
+import com.hozzi.order.domain.store.repo.MenuRepo;
+import com.hozzi.order.domain.store.repo.StoreRepo;
+import com.hozzi.order.domain.store.service.impl.StoreServiceImpl;
+import com.hozzi.order.domain.user.repo.UserRepo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StoreServiceTest {
+    StoreService storeService;
 
+    private StoreRepo storeRepo = Mockito.mock(StoreRepo.class);
+    private UserRepo userRepo = Mockito.mock(UserRepo.class);
+    @BeforeEach
+    public void SetupTest() {
+        storeService = new StoreServiceImpl(storeRepo, userRepo);
+    }
+    @Test
+    void readStore_Normal_Success() {
+    }
     @Test
     void readStore() {
     }
-
+    @Test
+    void createStore_Normal_Success() {
+    }
     @Test
     void createStore() {
     }
-
     @Test
-    void updateStore() {
+    void updateStore_Normal_Success() {
+    }
+    @Test
+    void updateStore_NotExistStoreId_Exception() {
+    }
+    @Test
+    void updateStore_NotExistUserId_Exception() {
     }
 }
