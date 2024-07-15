eed8efc492ee7114390d6ebfead4fcfbbd052a30
HSCHOO <57245883+HoSungChoo@users.noreply.github.com>
2024-07-15 10:41:06 +0900

    업로드를 위한 mapper 변경

--- order/src/main/java/com/hozzi/order/domain/order/mapper/OrderManageMapper.java
+++ order/src/main/java/com/hozzi/order/domain/order/mapper/OrderManageMapper.java
@@ -3,12 +3,15 @@ package com.hozzi.order.domain.order.mapper;
 import com.hozzi.order.domain.order.dto.UpdateOrderByAdminOutDTO;
 import com.hozzi.order.domain.order.dto.UpdateOrderByCustomOutDTO;
 import com.hozzi.order.domain.order.dto.UpdateOrderByOwnerOutDTO;
+import com.hozzi.order.domain.order.entity.Order;
 import com.hozzi.order.domain.order.entity.OrderManage;
+import com.hozzi.order.domain.user.entity.User;
+import com.hozzi.order.domain.user.entity.Wallet;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", imports = {Order.class, User.class},unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface OrderManageMapper {
     UpdateOrderByCustomOutDTO toUpdateOrderByCustomOutDTO(OrderManage orderManage);
     UpdateOrderByOwnerOutDTO toUpdateOrderByOwnerOutDTO(OrderManage orderManage);
